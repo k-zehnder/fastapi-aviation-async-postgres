ebb4e5be09e40515042ab6841b2322aa48c976f8
k-zehnder <kjzehnder3@gmail.com>
2022-02-11 10:11:58 -0800

    cleanup Point, Area models

--- project/app/models.py
+++ project/app/models.py
@@ -154,27 +154,3 @@ class Area(BaseModel):
         return (coord for coord in (self.sw.lat, self.sw.lon,
                                        self.ne.lat, self.ne.lon))
     
-# # FR models
-# class Point:
-#     def __init__(self, lat: float, lon: float):
-#         self.lat = lat
-#         self.lon = lon
-
-#     def __str__(self) -> str:
-#         return '({}, {})'.format(self.lat, self.lon)
-
-# class Area:
-#     def __init__(self, sw: Point, ne: Point):
-#         self.southwest_lat = sw.lat
-#         self.southwest_lon = sw.lon
-#         self.northeast_lat = ne.lat
-#         self.northeast_lon = ne.lon
-
-#     def __str__(self) -> str:
-#         """Allows to unpack data this way: *area"""
-#         return '{}, {}, {}, {}'.format(self.southwest_lat, self.southwest_lon,
-#                                        self.northeast_lat, self.northeast_lon)
-
-#     def __iter__(self):
-#         return (coord for coord in (self.southwest_lat, self.southwest_lon,
-#                                     self.northeast_lat, self.northeast_lon))
---
+++ project/app/persist_to_database.py
@@ -0,0 +1,77 @@
+import json
+from os import remove
+from typing import Dict, List
+
+import sqlmodel
+from sqlmodel import create_engine, SQLModel, Session, select
+import asyncio
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import *
+from db import build_uri
+# from . import crud
+
+from dataclass.flightradar.api import API
+from dataclass.data_class import Data
+
+from hero_models import Team, TeamCreate
+
+
+
+async def async_main(data):
+    engine = create_async_engine(
+        "postgresql+asyncpg://postgres:password@localhost/foo",
+        echo=False,
+    )
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+    # expire_on_commit=False will prevent attributes from being expired
+    # after commit.
+    async_session = sessionmaker(
+        engine, expire_on_commit=True, class_=AsyncSession
+    )
+    
+    counts = 0
+
+    async with async_session() as session:
+        async with session.begin():
+            """
+            This is where you add data.
+            """
+            r1 = Response(
+                name="controller1",
+                time_created=datetime.datetime.now())
+            
+            print(data.keys()) # time, detailed
+            print(data["detailed"])
+            
+            counts += 1
+            
+            if counts > 1:
+                pass
+            
+            session.add(r1)
+            await session.commit()
+        # NOTE: this must go outside inner session.begin() block
+        await session.refresh(r1)
+            
+    # for AsyncEngine created in function scope, close and
+    # clean-up pooled connections
+    await engine.dispose()
+    
+    
+if __name__ == "__main__":
+    print()
+
+    # LIVE DATA
+    data = Data()
+    data = data.run()
+    print(data)
+    
+    asyncio.run(async_main(data))
