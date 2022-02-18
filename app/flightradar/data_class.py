f108e9f575c6f6ac9484c4fe298fd02899900675
k-zehnder <kjzehnder3@gmail.com>
2022-02-18 07:33:07 -0800

    comment cleanup

--- app/flightradar/data_class.py
+++ app/flightradar/data_class.py
@@ -18,16 +18,11 @@ class Data:
         self.parser = Parser()
         self.briefs = []
         self.detailed = []  
-        # self.point1 = Point(**{"lat" : 59.06, "lon" : 50.00}) # RUSSIA?
-        # self.point2 = Point(**{"lat" : 30.97, "lon" : 36.46})
-        self.point1 = Point(**{"lat" : 37.8, "lon" : 37.6}) # PTOWN?
-        self.point2 = Point(**{"lat" : -121.90, "lon" : -121.78})
+        self.point1 = Point(**{"lat" : 59.06, "lon" : 50.00}) # RUSSIA?
+        self.point2 = Point(**{"lat" : 30.97, "lon" : 36.46})
+        # self.point1 = Point(**{"lat" : 37.8, "lon" : 37.6}) # PTOWN?
+        # self.point2 = Point(**{"lat" : -121.90, "lon" : -121.78})
         self.mapp = Area(**{"sw" : self.point1, "ne" : self.point2})
-        
-        # PTOWN
-        # {"lat" : 37.8, "lon" : 37.6} 
-        # {"lat" : -121.90, "lon" : -121.78}
-        
         
     def get_area(self, area: Area) -> List[BriefFlightCreate]:
         """Returns all available flights within the specified area."""
